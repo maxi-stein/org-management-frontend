fbab72dc2c2b869e59cc62f39505eeeaef3bba5b
maximiliano-stein <maxi_stein23@hotmail.com>
2024-11-26 13:45:43 -0300

    company logo component created

---
+++ src/components/CompanyLogo.tsx
@@ -0,0 +1,30 @@
+export const CompanyLogo: React.FC = () => {
+  return (
+    <div
+      style={{
+        height: "80px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          height: "64px",
+          width: "64px",
+          backgroundColor: "white",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="../../public/organization-chart.png"
+          alt="logo"
+          style={{ width: "40px" }}
+        />
+      </div>
+    </div>
+  );
+};
