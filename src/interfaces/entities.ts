f42b8455ed6f109e7920bdbfdc9749bb3f10bce7
maximiliano-stein <maxi_stein23@hotmail.com>
2024-11-26 16:07:43 -0300

    updated entities

--- src/interfaces/entities.ts
+++ src/interfaces/entities.ts
@@ -1,7 +1,38 @@
-export type BffEntity = Area;
+export type BffEntity = Area | Department | Position | User;
 
 export interface Area {
   _id: string;
   name: string;
-  departments: string[];
+  departments: Department[];
+}
+
+export interface Department {
+  _id: string;
+  name: string;
+  description: string;
+  head: User;
+}
+
+export interface Position {
+  _id: string;
+  title: string;
+  level: string | null;
+}
+
+export interface Role {
+  _id: string;
+  name: string;
+}
+
+export interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: Role;
+  supervisedEmployees: User[];
+  phone: string;
+  bornDate: Date;
+  isActive: boolean;
+  position: Position | null;
 }
